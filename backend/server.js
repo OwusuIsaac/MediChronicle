5a6a7e8f96b5948f3b7d1e5cae5d70474ebc58a4
thabitfuad <thabitfuad@nyu.edu>
2024-04-25 15:37:48 +0400

    frontend and backend separate
    frontend and backend separate folders with login, logout, register, and role specific displays implemented

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+const port = 3001; // Make sure this does not conflict with your React app's port
+
+// Body parsing middleware to parse JSON
+app.use(express.json());
+app.use(cors());
+
+// MongoDB connection string, replace '<password>', '<username>', and '<dbname>' with your actual details
+const mongoURI =
+  "mongodb+srv://admin:mediadmin@cluster0.3qsyp1b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Connect to MongoDB
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+// Routes
+// Ensure that the userRoutes are implemented in './routes/users'
+const userRoutes = require("./routes/users"); // Make sure the path to your routes is correct
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
